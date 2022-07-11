4056ebbb7a041775277470a05481d2d137073882
Aatman <aatmanbhoraniya12@gmail.com>
2022-07-11 18:25:06 +0530

    banner and ip CHANGES

--- sextant/src/index.js
+++ sextant/src/index.js
@@ -11,6 +11,61 @@ root.render(
   </React.StrictMode>
 );
 
+class banner extends React.Component{
+  render(){
+    return(
+      <h1>Cisco Project</h1>
+    );
+
+
+  }
+
+}
+
+function Contacts() {
+  return <div className="Contacts" />;
+}
+
+function Chat() {
+  return <div className="Chat" />;
+}
+
+function SplitPane(props) {
+  return (
+    <div className="SplitPane">
+      
+      <div className="SplitPane-left">
+        This is user's public IP.
+        
+      
+        {props.left}
+        
+      
+      </div>
+      <div className="SplitPane-right">
+        This is latency information for Pylon.
+        {props.right}
+        
+      </div>
+    </div>
+  );
+}
+
+function App() {
+  return (
+    <SplitPane
+      left={
+        <Contacts />
+      }
+      right={
+        <Chat />
+      } />
+  );
+}
+
+
+
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
